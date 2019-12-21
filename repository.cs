857c7c1c2b0f936fd31582d3e1f31c9a181d56ac
romarikid <57034453+romarikid@users.noreply.github.com>
2019-12-21 16:59:45 +0300

    repository class

---
+++ repository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5
+{
+    public class repository
+    {
+        public void GetBookbyId(int id)
+        {
+            FileStream Books = new FileStream("Ссылка на файл", FileMode.Open);
+            StreamReader reader = new StreamReader(Books);
+            Console.WriteLine(reader.ReadToEnd());
+            reader.Close();
+        }
+    }
+}
