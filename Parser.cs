5f9ce5c885a25c239f2654d89cccbdde35407fd7
skazka <kiranixis@gmail.com>
2020-01-02 15:22:14 +0300

    first version

---
+++ Parser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+public class Parser
+{
+    public string[] Parsing(string str)
+    {
+        string[] lines = str.Split('\n');
+        string[,] result = new string[4, lines.Length - 1]; //здесь будет результат
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string[] lineParts = lines[i].Split(' ');
+            for (int j = 0; j < 4; j++)
+                result[i - 1, j] = lineParts[j];
+        }
+        return result;
+    }
+}
